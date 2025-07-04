e8dbd7fc295b357e0abfa11d5b06666c2f14d307
DevWithDroid <104615751+dipalijadav301@users.noreply.github.com>
2025-07-04 13:16:49 +0530

    Update PostRepository.kt
    Update and Maintain New Dependency.

--- app/src/main/java/com/example/practicaltask/dataService/PostRepository.kt
+++ app/src/main/java/com/example/practicaltask/dataService/PostRepository.kt
@@ -11,7 +11,7 @@ class PostRepository {
         val response = RetrofitClient.apiService.getPosts(
             page = page,
             body = RequestBodyDto(),
-            authHeader = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJfaWQiOiI1NzgiLCJuYW1lIjoiVHVzaGFyIE0iLCJjb3VudHJ5Q29kZSI6Iis5MSIsImVtYWlsIjoiIiwicGhvbmUiOjg5ODA5NjkxNzUsImRlc2NyaXB0aW9uIjoiSGV5LCBJJ20gaW4gVGVwbm90IiwidGFncyI6IiIsInByb2ZpbGUiOiJ1cGxvYWRzL3Byb2ZpbGUvMzY2Mzk2M0YtMkVDMC00MDhCLUFBODctMDU4QTRFRUMxRjg4LTIwODEtMDAwMDAyNkU3RkJCOTVBRC5qcGVnIiwib3RwU2VuZENvdW50IjoxLCJsYXN0T3RwU2VuZERhdGUiOiIyMDI0LTEyLTMxVDEyOjQzOjM1LjAwMloiLCJpc1VzZXJWZXJpZmllZCI6dHJ1ZSwiaXNCYW5uZWQiOmZhbHNlLCJkYXRlT2ZCaXJ0aCI6IjE5OTYtMDEtMDFUMDA6MDA6MDAuMDAwWiIsImxpbmsiOiIiLCJpc0RlbGV0ZWQiOmZhbHNlLCJvbGRQaG9uZSI6bnVsbCwidXNlclR5cGUiOiJVc2VyIiwiY3JlYXRlQXQiOiIyMDI0LTA5LTMwVDEwOjAwOjA0LjQ2NVoiLCJ1cGRhdGVBdCI6IjIwMjUtMDUtMTlUMDc6MjQ6MDMuNTQ0WiIsImJpb1VwZGF0ZSI6IjIwMjUtMDQtMTJUMDk6Mzk6MTkuNzE2WiIsIl9fdiI6MCwiaXNPdHBWZXJpZmllZCI6dHJ1ZX0sImlhdCI6MTc0ODMyNDc4M30.sP_YP4_C23WB2zsRYy-4qs96dDmyKql45ebhIV1K1lE"
+            authHeader = "your Auth key"
         )
         if (!response.isSuccessful) {
             Log.e(
